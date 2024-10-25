469cc95c8d6e9ede83db352db3cdd8db9b63f5ef
Swonk12 <polruizmarin@gmail.com>
2024-10-25 15:10:28 +0200

    Animacions jugador.

--- Player.gd
+++ Player.gd
@@ -6,7 +6,9 @@ var screen_size # Mida de la finestra de joc.
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	hide()
 	screen_size = get_viewport_rect().size
+
 
 func _process(delta):
 	var velocity = Vector2.ZERO # Vector de moviment del jugador.
@@ -16,7 +18,7 @@ func _process(delta):
 		velocity.x -= 1
 	if Input.is_action_pressed("move_down"):
 		velocity.y += 1
-	if Input.is_action_pressed("move_up"): 
+	if Input.is_action_pressed("move_up"):
 		velocity.y -= 1
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
@@ -26,3 +28,11 @@ func _process(delta):
 	position += velocity * delta
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
+	
+	if velocity.x != 0:
+		$AnimatedSprite.animation = "walk"
+		$AnimatedSprite.flip_v = false
+		$AnimatedSprite.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite.animation = "up"
+		$AnimatedSprite.flip_v = velocity.y > 0
